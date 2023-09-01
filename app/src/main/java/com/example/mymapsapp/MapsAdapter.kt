7dd9f75aaa8e1c2d07bf8007d1a1c3938646d9a4
Nikita Lytvyn <litvinnik1@gmail.com>
2023-09-01 17:28:38 +0300

    first commit

---
+++ app/src/main/java/com/example/mymapsapp/MapsAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.mymapsapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymapsapp.models.UserMap
+
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>) : RecyclerView.Adapter<MapsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = userMaps.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userMap = userMaps[position]
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
